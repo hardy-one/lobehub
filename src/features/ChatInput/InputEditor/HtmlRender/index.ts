ac0b69834d7d8e761efb46d27b27c50af9e01b50
Hardy <yimingmail@yandex.com>
2026-08-27 23:58:44 +0800

    ♻️ refactor(editor): add Markdown source handling to the chat input

---
+++ src/features/ChatInput/InputEditor/HtmlRender/index.ts
@@ -0,0 +1,8 @@
+export {
+  $createHtmlRenderNode,
+  $isHtmlRenderNode,
+  HtmlRenderNode,
+  type SerializedHtmlRenderNode,
+} from './HtmlRenderNode';
+export { HtmlRenderPlugin } from './HtmlRenderPlugin';
+export { default as ReactHtmlRenderPlugin } from './ReactHtmlRenderPlugin';

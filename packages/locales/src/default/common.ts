78ec1aeb286a07ba28fbc7cd00f25e0c047a1af5
Hardy <yimingmail@yandex.com>
2026-09-02 13:08:07 +0800

    ✨ clarify mobile navigation

--- packages/locales/src/default/common.ts
+++ packages/locales/src/default/common.ts
@@ -605,6 +605,7 @@ export default {
   'taskTemplate.section.title': 'Try these scheduled tasks',
   'tab.image': 'Image',
   'tab.audio': 'Audio',
+  'tab.agents': 'Agents',
   'tab.chat': 'Chat',
   'recents': 'Recents',
   'tab.community': 'Community',

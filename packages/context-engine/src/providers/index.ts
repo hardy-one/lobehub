6196560726ca407fc4b171a8ea05a49c0fd05bea
Hardy <yimingmail@yandex.com>
2026-09-09 11:00:43 +0800

    ✨ feat(prompt): keep working directory guidance in lean mode

--- packages/context-engine/src/providers/index.ts
+++ packages/context-engine/src/providers/index.ts
@@ -62,6 +62,7 @@ export { SystemRoleInjector } from './SystemRoleInjector';
 export { TaskManagerContextInjector } from './TaskManagerContextInjector';
 export { TodoInjector } from './TodoInjector';
 export {
+  LEAN_LOCAL_SYSTEM_WORKING_DIRECTORY_PROMPT,
   LEAN_TOOL_USAGE_POLICY,
   selectToolPromptManifests,
   ToolSystemRoleProvider,
